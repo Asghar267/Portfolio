53e82a02394610d9378941a1184aad0398e6c5b5
Asghar Abbasi <asgharabbasi267@gmail.com>
2023-05-22 23:14:28 +0500

    created supper user and contact model

--- Portfolio_site/Portfolio/apps.py
+++ Portfolio_site/Portfolio/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PorfolioConfig(AppConfig):
+class PortfolioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Porfolio'
+    name = 'Portfolio'
